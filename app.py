1566e9138a903acb1a2f7cc3caecf047aa85692e
david barkhuizen <david.barkhuizen@gmail.com>
2020-11-25 15:30:21 +0200

    insecure

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, Response
+import cv2
+import time
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            print('FAILED')
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# ----------------
+
+# cv2.namedWindow("test")
+
+# img_counter = 0
+
+# while True:
+#     ret, frame = cam.read()
+#     if not ret:
+#         print("failed to grab frame")
+#         break
+#     cv2.imshow("test", frame)
+
+#     k = cv2.waitKey(1)
+#     if k%256 == 27:
+#         # ESC pressed
+#         print("Escape hit, closing...")
+#         break
+#     elif k%256 == 32:
+#         # SPACE pressed
+#         img_name = "opencv_frame_{}.png".format(img_counter)
+#         cv2.imwrite(img_name, frame)
+#         print("{} written!".format(img_name))
+#         img_counter += 1
+
+# cam.release()
+
+# cv2.destroyAllWindows()
