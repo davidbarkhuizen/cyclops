74a8c67b4038b4015d334428614e34d82e45edcb
david barkhuizen <david.barkhuizen@gmail.com>
2020-11-27 12:10:52 +0200

    fix

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import time
 
 import pyaudio
 
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+# p = pyaudio.PyAudio()
+# info = p.get_host_api_info_by_index(0)
+# numdevices = info.get('deviceCount')
+# for i in range(0, numdevices):
+#         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+#             print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 # --------------------------------------------
 
@@ -45,7 +45,7 @@ stream = audio.open(format=FORMAT,
     channels=CHANNELS, 
     rate=RATE, 
     input=True, 
-    input_device_index=10, 
+    input_device_index=0, 
     frames_per_buffer=CHUNK)
 
 print("recording...")
